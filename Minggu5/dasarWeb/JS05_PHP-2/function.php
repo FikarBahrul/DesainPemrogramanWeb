211766e5f3379f2f914f079f35fd7fdb2d2e827b
FikarBahrul <fikarbahrul99@gmail.com>
2025-09-30 05:16:00 +0700

    Checkpoint PHP2

--- Minggu5/dasarWeb/JS05_PHP-2/function.php
+++ Minggu5/dasarWeb/JS05_PHP-2/function.php
@@ -1,6 +1,6 @@
 <?php
     //membuat fungsi
-    function perkenalan($nama, $salam){
+    function perkenalan($nama, $salam="Assalamualaikum"){
         echo $salam . ", ";
         echo "Perkenalkan, nama saya " . $nama . ".<br/>";
         echo "Senang berkenalan dengan Anda<br/>";
@@ -11,6 +11,6 @@
     $saya = "Elok";
     $ucapanSalam = "Selamat pagi";
 
-    //memanggil lagi
-    perkenalan($saya, $ucapanSalam);
+    //memanggil lagi tanpa parameter kedua
+    perkenalan($saya);
 ?>
