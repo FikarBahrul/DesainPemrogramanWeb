c197032eb1fa2dce8b915412f7491fa78ff36eda
FikarBahrul <fikarbahrul99@gmail.com>
2025-09-30 05:31:12 +0700

    Checkpoint PHP2

--- Minggu5/dasarWeb/JS05_PHP-2/function_return.php
+++ Minggu5/dasarWeb/JS05_PHP-2/function_return.php
@@ -4,7 +4,14 @@
         $umur = $thn_sekarang - $thn_lahir;
         return $umur;
     }
+    function perkenalan($nama, $salam="Assalamualaikum"){
+        echo $salam . ", ";
+        echo "Perkenalkan, nama saya " . $nama . ".<br/>";
 
-    echo "Umur saya adalah ". hitungUmur(1988, 2023) . " tahun";
-    //isi sesuai dengan tahun lahir kalian
+        //memanggil fungsi lain
+        echo "Saya berusia ". hitungUmur(1988, 2023) . " tahun<br>";
+        echo "Senang berkenalan dengan Anda<br/>";
+    }
+    //memanggil fungsi perkenalan
+    perkenalan("Elok");
 ?>
