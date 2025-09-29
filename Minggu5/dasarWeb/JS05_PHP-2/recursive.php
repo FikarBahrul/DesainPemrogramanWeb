469f25fc0fbabcceb597d226babd0b7b08039b60
FikarBahrul <fikarbahrul99@gmail.com>
2025-09-30 06:08:43 +0700

    Checkpoint PHP2

--- Minggu5/dasarWeb/JS05_PHP-2/recursive.php
+++ Minggu5/dasarWeb/JS05_PHP-2/recursive.php
@@ -1,8 +1,11 @@
 <?php
-    function tampilkanHaloDunia(){
-        echo "Halo Dunia!";
+    function tampilkanAngka(int $jumlah, int $indeks = 1) {
+        echo "Perulangan ke-{$indeks} <br>";
 
-        tampilkanHaloDunia();
+        //panggil diri sendiri selama $indeks <= $jumlah
+        if ($indeks < $jumlah) {
+            tampilkanAngka($jumlah, $indeks + 1);
+        }
     }
-    tampilkanHaloDunia();
+    tampilkanAngka(20);
 ?>
